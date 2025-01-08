3708edb553453ad54be359a2411d1213d50220f7
Sarangsh <sarangshnandi@gmail.com>
2025-01-08 16:40:24 -0600

    Added a new file

---
+++ App.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.title('Test app for Github')
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('/Users/sarangsh/Documents/GitHub/Practice-Github/Neymar.jpeg',caption='Neymar')
+
+with col2:
+    st.write('''Neymar is one of my favorite players in the world. 
+    I am disappointed when he is not playing.''')
